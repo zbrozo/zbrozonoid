465c52afe5b88b37a4b1854a555d93cb5101c92a
tomek <tomasz.zbrozek@o2.pl>
2019-01-02 21:30:32 +0100

    AssemblyInfo

--- zbrozonoid/Properties/AssemblyInfo.cs
+++ zbrozonoid/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("tomek")]
+[assembly: AssemblyCopyright ("tomasz.zbrozek")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
