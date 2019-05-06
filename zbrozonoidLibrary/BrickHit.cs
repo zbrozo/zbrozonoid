3d54eae9bfd5d66472b8fa3b7c8df0bf67296ce3
Tomasz Zbrożek <tzbrozek@future-processing.com>
2019-05-06 12:36:30 +0200

    BallStateMachine

---
+++ zbrozonoidLibrary/BrickHit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using zbrozonoidLibrary.Interfaces;
+
+namespace zbrozonoidLibrary
+{
+    public class BrickHit
+    {
+        public BrickHit(int number, IBrick brick)
+        {
+            Number = number;
+            Brick = brick;
+        }
+
+        public int Number { get; set; }
+        public IBrick Brick { get; set; }
+    }
+}
