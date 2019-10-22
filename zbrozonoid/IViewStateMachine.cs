c179c88d42829017bcd9b74b93fab538a0f34016
Tomasz Zbrożek <tzbrozek@future-processing.com>
2019-10-22 09:12:14 +0200

    Autofac used for visualization layer

---
+++ zbrozonoid/IViewStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using zbrozonoidEngine.Interfaces;
+
+namespace zbrozonoid.Views
+{
+    public interface IViewStateMachine
+    {
+        bool IsMenuState { get; }
+
+        void GotoMenu();
+
+        void GotoPlay();
+
+        void GotoGameOver();
+
+        void Action();
+
+        void Transitions(IGame game);
+    }
+}
