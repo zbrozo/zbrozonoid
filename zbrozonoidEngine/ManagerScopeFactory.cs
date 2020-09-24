b2cc6744ea0c9c352ced4dc14b85ae5727114717
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2020-09-24 11:57:30 +0200

    Add ManagerScopeFactory

---
+++ zbrozonoidEngine/ManagerScopeFactory.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace zbrozonoidEngine
+{
+    public class ManagerScopeFactory
+    {
+        public ManagerScopeFactory()
+        {
+        }
+
+        public ILifetimeScope Create()
+        {
+            var builder = new ContainerBuilder();
+            return builder.Build();
+        }
+    }
+}
