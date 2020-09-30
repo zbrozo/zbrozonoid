90386552fa0903debfb9f5d5cddba6fcd7fb5268
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2020-09-30 14:58:00 +0200

    Add SharePadPosition class

---
+++ zbrozonoid/SharePadPosition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+namespace zbrozonoid
+{
+    public class SharePadPosition : IDisposable
+    {
+        private readonly MemoryMappedFile file;
+
+        public SharePadPosition()
+        {
+            file = MemoryMappedFile.CreateFromFile("/tmp/zbrozonoid", FileMode.OpenOrCreate, "pad");
+        }
+
+        public void Write()
+        {
+        }
+
+        public void Dispose()
+        {
+            file?.Dispose();
+        }
+    }
+}
