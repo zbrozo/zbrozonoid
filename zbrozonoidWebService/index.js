7d35848893829e5295c3f01f3a5a85d6bf5fe490
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2024-06-05 15:52:27 +0200

    webservice updated with port variable

--- zbrozonoidWebService/index.js
+++ zbrozonoidWebService/index.js
@@ -29,5 +29,7 @@ app.put('/api/values/:id', function(req, res) {
     res.end();
 });
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
