2f0008ba4e4e88941b3ccc072d7ee7e0e66e2eb4
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2024-06-05 15:51:08 +0200

    Rest put and get modified

--- zbrozonoid/WebClient.cs
+++ zbrozonoid/WebClient.cs
@@ -1,4 +1,5 @@
-﻿using RestSharp;
+﻿using System;
+using RestSharp;
 
 namespace zbrozonoid
 {
@@ -8,22 +9,21 @@ namespace zbrozonoid
 
         public WebClient(string url)
         {
+            Console.Write(url);
             client = new RestClient(url);
         }
 
         public void Put(int id, string data)
         {
-            var request = new RestRequest("values/" + id, Method.PUT);
-            request.AddJsonBody(data);
-            var response = client.Execute(request);
-            var status = response.StatusCode;
+            var request = new RestRequest("values/" + id, Method.Put);
+            request.AddStringBody(data, ContentType.Json);
+            client.Execute(request);
         }
 
         public string Get(int id)
         {
-            var request = new RestRequest("values/" + id, Method.GET);
-            IRestResponse<string> response = client.Execute<string>(request);
-            client.Execute(request);
+            var request = new RestRequest("values/" + id, Method.Get);
+            var response = client.Execute(request);
             return response.Content;
         }
     }
